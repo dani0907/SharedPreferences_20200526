e9330639dcab2149c9cbc669cd5258c746775522
dy696786 <dy696786@gmail.com>
2020-05-26 13:06:21 +0900

    비밀번호 입력 화면 그리기

--- app/src/main/java/com/example/sharedpreferences_20200526/SignUpActivity.java
+++ app/src/main/java/com/example/sharedpreferences_20200526/SignUpActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.database.DatabaseUtils;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -39,13 +40,18 @@ public class SignUpActivity extends BaseActivity {
 //                @를 포함 +6글자 이상 => 이메일로 인정.
                 if(input.contains("@")&& input.length()>=6){
                     binding.emailCheckResultTxt.setText("사용해도 좋은 이메일입니다.");
+
+//                    하늘색으로 글씨 색 변경.
+                    binding.emailCheckResultTxt.setTextColor(Color.parseColor("#2767e3"));
                 }
                 else{
                     if(input.length()==0){
                         binding.emailCheckResultTxt.setText("이메일을 입력해주세요.");
+                        binding.emailCheckResultTxt.setTextColor(Color.parseColor("#A0A0A0"));
                     }
                     else{
                         binding.emailCheckResultTxt.setText("이메일 양식으로 입력해주세요.");
+                        binding.emailCheckResultTxt.setTextColor(Color.RED);
                     }
                 }
             }
